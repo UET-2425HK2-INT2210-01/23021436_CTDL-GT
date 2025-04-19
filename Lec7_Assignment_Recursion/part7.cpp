52df34bb9244ce51d073ece14ae8c8796ee3fe18
Nguyen Dang Sang <23021436@vnu.edu.vn>
2025-04-19 19:38:40 +0700

    Create part7.cpp

---
+++ Lec7_Assignment_Recursion/part7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// Hàm đảo ngược số nguyên n
+int reverseNumbers(int n, int revNum)
+{
+    if(n == 0)
+    { //  n = 0 thì return revNum
+        return revNum;
+    }
+    return reverseNumbers( n / 10, revNum * 10 + (n % 10) ); // Gọi đệ quy đuôi
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << reverseNumbers(n, 0);
+    return 0;
+}
