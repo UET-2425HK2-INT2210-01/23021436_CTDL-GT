03058651686b84b043c584572107f051c94188ad
Nguyen Dang Sang <23021436@vnu.edu.vn>
2025-04-19 19:36:57 +0700

    Create part4.cpp

---
+++ Lec7_Assignment_Recursion/part4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// Hàm tính lũy thừa x ^ n
+int luyThua(int x, int n)
+{
+    if(n == 0)
+    { //  n = 0 thì return 1
+        return 1;
+    }
+    return x * luyThua(x, n - 1); // Gọi đệ quy
+}
+
+int main()
+{
+    int x, n;
+    cin >> x >> n;
+    cout << luyThua(x, n);
+    return 0;
+}
