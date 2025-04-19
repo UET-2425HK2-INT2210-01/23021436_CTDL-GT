202af0354ceaacb6ddb3c527d53c4c1602b978a7
Nguyen Dang Sang <23021436@vnu.edu.vn>
2025-04-19 19:35:41 +0700

    Create part2.cpp

---
+++ Lec7_Assignment_Recursion/part2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// Hàm tính giai thừa
+long long factorial(int n)
+{
+    if(n == 1 || n == 0)
+    { //  n = 0 | n= 1 thì return 1
+        return 1;
+    }
+    return n * factorial(n - 1); // Gọi đệ quy
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
