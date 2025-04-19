0c973ad7b7b55be8756b286ce861e550f2094c77
Nguyen Dang Sang <23021436@vnu.edu.vn>
2025-04-19 19:35:08 +0700

    Create part1.cpp

---
+++ Lec7_Assignment_Recursion/part1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// Hàm tính tổng từ 1 -> n
+int sum(int n)
+{
+    if(n == 1)
+    { //  n = 1 thì return 1
+        return 1;
+    }
+    return n + sum(n - 1); // Gọi đệ quy
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sum(n);
+    return 0;
+}
