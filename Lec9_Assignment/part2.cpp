0ea922fc34cf01adcaeadebc9d5897ec0547c64c
Nguyen Dang Sang <23021436@vnu.edu.vn>
2025-04-25 03:17:50 +0700

    Create part2.cpp

---
+++ Lec9_Assignment/part2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool trySum( vector<int>& a, int x, int y)
+{;
+	for (int i = 0; i < x ; i++)
+	{
+		if (y >= a[i])
+		{
+			y = y - a[i];
+		}
+		else
+		{
+			y = y;
+		}
+	}
+	
+	if (y == 0)
+	{
+		cout << "YES" << endl;
+		return true;
+	}
+	else
+	{
+		cout << "NO" << endl;
+		return false;
+	}
+}
+
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+	vector<int> a(n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end(), greater<int>());
+	trySum(a, n, x);
+  
+	return 0;
+}
