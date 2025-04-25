218773da98393abc5f1a17ed4d14ce12f11c7689
Nguyen Dang Sang <23021436@vnu.edu.vn>
2025-04-25 16:35:31 +0700

    Update part2.cpp

--- Lec9_Assignment/part2.cpp
+++ Lec9_Assignment/part2.cpp
@@ -4,45 +4,59 @@
 
 using namespace std;
 
-bool trySum( vector<int>& a, int x, int y)
+bool trySum(vector<int>& a, int y) 
 {
-	for (int i = 0; i < x ; i++)
+    for (int i = 0; i < a.size(); i++) 
+    {
+        if (y >= a[i]) 
 	{
-		if (y >= a[i])
-		{
-			y = y - a[i];
-		}
-		else
-		{
-			y = y;
-		}
-	}
-	
-	if (y == 0)
-	{
-		cout << "YES" << endl;
-		return true;
-	}
-	else
-	{
-		cout << "NO" << endl;
-		return false;
-	}
+            y -= a[i];
+        }
+    }
+
+    return y == 0;
 }
 
-int main()
+void dequy(vector<int> a, int y) 
 {
-	int n, x;
-	cin >> n >> x;
-	vector<int> a(n);
-	
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
+    if (y == 0) 
+    {
+        cout << "YES" << endl;
+        return;
+    }
 
-	sort(a.begin(), a.end(), greater<int>());
-	trySum(a, n, x);
-  
-	return 0;
+    if (a.empty()) 
+    {
+        cout << "NO" << endl;
+        return;
+    }
+
+    if (trySum(a, y)) 
+    {
+        cout << "YES" << endl;
+        return;
+    }
+
+    // Xoá phần tử đầu tiên và gọi đệ quy lại
+    a.erase(a.begin());
+    dequy(a, y);
+}
+
+int main() 
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> a[i];
+    }
+
+    // Sắp xếp giảm dần để ưu tiên thử phần tử lớn trước
+    sort(a.begin(), a.end(), greater<int>());
+
+    dequy(a, x);
+
+    return 0;
 }
